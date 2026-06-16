b86b2136a22db15e427989b415fef8ffa28760ca
alimoradikhah1993-tech <alimoradikhah1993@gmail.com>
2026-06-16 14:44:09 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
 import os
-import asyncio
 from telegram import Bot
 
 TOKEN = os.environ.get("TOKEN")
@@ -7,8 +6,5 @@ CHAT_ID = os.environ.get("CHAT_ID")
 
 bot = Bot(token=TOKEN)
 
-async def _send(text):
-    await bot.send_message(chat_id=CHAT_ID, text=text)
-
 def send_report(text):
-    asyncio.run(_send(text))
+    bot.send_message(chat_id=CHAT_ID, text=text)
