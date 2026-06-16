e7c5ea69f33f7589b310efe089be9810b1498bb4
alimoradikhah1993-tech <alimoradikhah1993@gmail.com>
2026-06-16 16:36:20 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
-from telegram import Bot
 import asyncio
+from telegram import Bot
 
 TOKEN = "YOUR_BOT_TOKEN"
 CHAT_ID = "YOUR_CHAT_ID"
@@ -15,16 +15,6 @@ def send_report(text):
     try:
         asyncio.run(_send(text))
     except RuntimeError:
-        # وقتی event loop بازه (Render / scheduler)
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
-        loop.run_until_complete(_send(text))import os
-from telegram import Bot
-
-TOKEN = os.environ.get("TOKEN")
-CHAT_ID = os.environ.get("CHAT_ID")
-
-bot = Bot(token=TOKEN)
-
-def send_report(text):
-    bot.send_message(chat_id=CHAT_ID, text=text)
+        loop.run_until_complete(_send(text))
