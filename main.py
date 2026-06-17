bb41ed0b44da3305dc8d6498d6c99c7b95e04058
alimoradikhah1993-tech <alimoradikhah1993@gmail.com>
2026-06-17 15:49:07 +0330

    Update main.py

--- main.py
+++ main.py
@@ -5,15 +5,18 @@ from telegram.ext import Application, CommandHandler, MessageHandler, ContextTyp
 TOKEN = os.getenv("BOT_TOKEN")
 
 if not TOKEN:
-    raise ValueError("BOT_TOKEN is not set")
+    raise RuntimeError("BOT_TOKEN is missing")
 
 
+# /start
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("🤖 ربات فعال شد")
+    await update.message.reply_text("🤖 ربات روشن شد و آماده است")
 
 
+# echo
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(update.message.text)
+    if update.message and update.message.text:
+        await update.message.reply_text(update.message.text)
 
 
 def main():
@@ -22,8 +25,13 @@ def main():
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 
-    print("Bot running...")
-    app.run_polling()
+    print("Bot is running...")
+
+    # IMPORTANT for Render
+    app.run_polling(
+        drop_pending_updates=True,
+        close_loop=False
+    )
 
 
 if __name__ == "__main__":
